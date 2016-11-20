bbbcb2248d30d7de28defa5f2d367b1827bb1569
LeKeb <wilhelm2803@gmail.com>
2016-11-20 17:06:13 +0200

    Add game project files

---
+++ desktop/src/com/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.Game;
+
+public class DesktopLauncher {
+	
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 360;
+		config.height = 640;
+		config.title = "The game";
+		new LwjglApplication(new Game(), config);
+	}
+	
+}
