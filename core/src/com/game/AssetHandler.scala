90ea5cb470060f11cac50aee4042f0d328ad11d0
LeKeb <wilhelm2803@gmail.com>
2016-11-22 16:40:30 +0200

    add Sounds to AssetHandler

--- core/src/com/game/AssetHandler.scala
+++ core/src/com/game/AssetHandler.scala
@@ -11,23 +11,33 @@ object AssetHandler {
 		val PLAYER = "player"
 		val NORMAL_PLATFORM = "normPlat"
 		val BOOST_PLATFORM = "boostPlat"
+		val BREAKABLE_PLATFORM = "breakPlat"
+		val BREAKABLE_PLATFORM_BROKEN = "breakPlatBroken"
 	}
 	
 	object Sound {
-		
+		val CRACK = "crack"
 	}
 	
 	private val textures: HashMap[String, Texture] = new HashMap[String, Texture]()
 	private val sounds: HashMap[String, Sound] = new HashMap[String, Sound]()
 	
 	private def loadTexture(file: String) = {
-		new Texture(Gdx.files.internal(file))
+		new Texture(Gdx.files.internal("Textures/" + file))
+	}
+	
+	private def loadSound(file: String) = {
+		Gdx.audio.newSound(Gdx.files.internal("Sounds/" + file))
 	}
 	
 	def loadAssets() = {
 		textures += Texture.PLAYER -> loadTexture("ball.png")
-		textures += Texture.NORMAL_PLATFORM -> loadTexture("NormalPlatform.png")
-		textures += Texture.BOOST_PLATFORM -> loadTexture("BoostPlatform.png")
+		textures += Texture.NORMAL_PLATFORM -> loadTexture("Platform1.png")
+		textures += Texture.BOOST_PLATFORM -> loadTexture("Platform3.png")
+		textures += Texture.BREAKABLE_PLATFORM -> loadTexture("Platform2.png")
+		textures += Texture.BREAKABLE_PLATFORM_BROKEN -> loadTexture("Platform2 broken.png")
+		
+		sounds += Sound.CRACK -> loadSound("cracking.wav")
 	}
 	
 	def getTexture(texture: String) = {
