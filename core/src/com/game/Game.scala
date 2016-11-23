88317cacfb23e3ef89b308b69451ab9961d434ea
LeKeb <wilhelm2803@gmail.com>
2016-11-23 11:52:14 +0200

    test commit

--- core/src/com/game/Game.scala
+++ core/src/com/game/Game.scala
@@ -5,34 +5,35 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Color
 
 class Game extends ApplicationAdapter {
-	
-	private var batch: SpriteBatch = _
-	private var game: GameWorld = _ 
-	private var camera: Camera = _
-	
-	override def create() = {
-		batch = new SpriteBatch
-		AssetHandler.loadAssets()
-		game = new GameWorld
-		camera = new Camera
-	}
-	
-	override def render() = {
-		
-		game.update(Gdx.graphics.getDeltaTime)
-		
-		camera.setPosition(Camera.renderWidth / 2 , Camera.renderHeight / 2 + game.getPlayerPos.y)
-		camera.update()
-		
-		batch.setProjectionMatrix(camera.getCamera.combined)
-		
-		Gdx.gl.glClearColor(0, 0.7.toFloat, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		game.draw(batch)
-		batch.end();
-	}
-	
+
+  private var batch: SpriteBatch = _
+  private var game: GameWorld = _
+  private var camera: Camera = _
+
+  override def create() = {
+    batch = new SpriteBatch
+    AssetHandler.loadAssets()
+    game = new GameWorld
+    camera = new Camera
+  }
+
+  override def render() = {
+
+    game.update(Gdx.graphics.getDeltaTime)
+
+    camera.setPosition(Camera.renderWidth / 2 , Camera.renderHeight / 2 + game.getPlayerPos.y)
+    camera.update()
+
+    batch.setProjectionMatrix(camera.getCamera.combined)
+
+    Gdx.gl.glClearColor(0, 0.7.toFloat, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    batch.begin();
+    game.draw(batch)
+    batch.end();
+  }
+
 }
--- core/src/com/game/objects/BoostPlatform.scala
+++ core/src/com/game/objects/BoostPlatform.scala
@@ -4,12 +4,12 @@ import com.game.AssetHandler
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion
 
 class BoostPlatform(x: Float, y: Float, w: Float, h: Float) extends Platform(x, y, w, h) {
-	
-	private val t = AssetHandler.getTexture(AssetHandler.Texture.BOOST_PLATFORM)
-	protected val tex = new AtlasRegion(t, 0, 0, t.getWidth, t.getHeight)
-	
-	def update(delta: Float) = {
-		
-	}
-  
+
+  private val t = AssetHandler.getTexture(AssetHandler.Texture.BOOST_PLATFORM)
+  protected val tex = new AtlasRegion(t, 0, 0, t.getWidth, t.getHeight)
+
+  def update(delta: Float) = {
+
+  }
+
 }
