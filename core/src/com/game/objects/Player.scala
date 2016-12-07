574ab5cf06d050bf047f40f550da374efdd0665e
maikki1 <anna-maija.rauramaa@aalto.fi>
2016-12-07 15:39:36 +0200

    liikkuvuus

--- core/src/com/game/objects/Player.scala
+++ core/src/com/game/objects/Player.scala
@@ -1,33 +1,59 @@
 package com.game.objects
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion
 import com.game.AssetHandler
 import com.badlogic.gdx.math.Vector2
 
 class Player(x: Float, y: Float, w: Float, h: Float) {
-  
+
   val tex = new AtlasRegion(AssetHandler.getTexture(AssetHandler.Texture.PLAYER), 0, 0, 256, 256)
-  
+
   private var xCoord = x
   private var yCoord = y
   private var width = w
   private var height = h
-  
+
   private var xx = 1.toFloat
-  
+
   def update(delta: Float) = {
-    xCoord += Math.sin(xx).toFloat * 20
-    xx += 0.5.toFloat
-    //Pitää varmistaa, ettei pelaaja pääse ruudun ulkopuolelle.
+    //Testipätkä alla.
+    // xCoord += Math.sin(xx).toFloat * 20
+    // xx += 0.5.toFloat
+
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+      move(-10.toFloat, 0)
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+      move(10.toFloat, 0)
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      move(0.toFloat, -10.toFloat)
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      move(0.toFloat, 10.toFloat)
+    }
+    //Ideana on tehdä alle metodit, joita kutsutaan edellä.
+
+    def jump = {
+      //Hyödynnä sin-funktiota välillä 0, pi
+    }
+
+    def move(moveSide: Float, moveUp: Float) = {
+      xCoord += moveSide
+      yCoord += moveUp
+    }
+    //Pitää varmistaa, ettei pelaaja pääse ruudun ulkopuolelle. 
   }
-  
+
   //Hitbox
-  
+
   def draw(batch: SpriteBatch) = {
     batch.draw(tex, xCoord, yCoord, width, height)
   }
-  
+
   def getPos = new Vector2(xCoord, yCoord)
-  
+
 }
